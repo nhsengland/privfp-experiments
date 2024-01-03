f0c43d797292b7accd845bd5c241694f292dccc1
Scarlett Kynoch <scarlett.kynoch1@nhs.net>
2024-01-03 15:29:54 +0000

    Add initial universal ner pipeline similar to exploration pages

---
+++ src/ner_pipeline/data_ingestion.py
@@ -0,0 +1,14 @@
+import json
+
+
+def load_llm_data():
+    """Laods in the llm dataset
+
+    Returns:
+        list: This is a list of a 1000 string outputs created using a large language model to
+              generate medical notes from a 1000 patients from a sunthea dataset.
+    """
+    with open("../../data/llm_dataset.json") as f:
+        data = json.load(f)
+
+    return data
---
+++ src/ner_pipeline/universal_ner.py
@@ -0,0 +1,61 @@
+import sys
+
+print(sys.path)
+
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.llms import LlamaCpp
+from langchain import LLMChain
+from langchain.prompts import PromptTemplate
+
+
+def upload_quantised_universal_ner(n_gpu_layers=1, n_batch=512):
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+    llm = LlamaCpp(
+        model_path="../../models/quantized_q4_1.gguf",
+        n_gpu_layers=n_gpu_layers,
+        n_batch=n_batch,
+        f16_kv=True,
+        callback_manager=callback_manager,
+        verbose=True,
+    )
+
+    return llm
+
+
+def get_universal_ner_entity(input_text, entity_name, llm):
+    # This strip the text of any special text characters \n etc.
+    prompt_template = """
+    USER: Text: {input_text}
+    ASSISTANT: I’ve read this text.
+    USER: What describes {entity_name} in the text?
+    ASSISTANT: (model's predictions in JSON format)
+    """
+    input_text = input_text.strip()
+
+    prompt = prompt_template.format_map(
+        {"input_text": input_text, "entity_name": entity_name}
+    )
+
+    output = llm(prompt)
+
+    return output
+
+
+def generate_patients_entities(data, entities):
+    llm = upload_quantised_universal_ner()
+    patients_entities = {}
+
+    for patient_num in range(len(data))[0:5]:
+        input_text = data[patient_num]
+        patient_entities = {}
+        for entity in entities:
+            output = get_universal_ner_entity(input_text, entity, llm)
+            patient_entities[entity] = output
+
+        patients_entities[patient_num] = {
+            "text": input_text,
+            "entities": patient_entities,
+        }
+
+    return patients_entities
