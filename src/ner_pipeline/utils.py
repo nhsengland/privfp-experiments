34f48f4b3af921598e36936a4bc4f7ece0a526e9
Scarlett Kynoch <114353573+scarlett-k-nhs@users.noreply.github.com>
2024-02-13 10:29:38 +0000

    Sk update readme (#9)

--- src/ner_pipeline/utils.py
+++ src/ner_pipeline/utils.py
@@ -1,12 +1,14 @@
 import pickle
 
 
-def save_patients_entities(patients_entities):
-    with open("./notebooks/extraction_module/data/patient_entities.pickle", "wb") as f:
+def save_patients_entities(patients_entities, filename):
+    folderpath = f"./notebooks/extraction_module/data/{filename}.pickle"
+    with open(folderpath, "wb") as f:
         pickle.dump(patients_entities, f)
 
 
-def load_patients_entities():
-    with open("./notebooks/extraction_module/data/patient_entities.pickle", "rb") as f:
+def load_patients_entities(filename):
+    folderpath = f"./notebooks/extraction_module/data/{filename}.pickle"
+    with open(folderpath, "rb") as f:
         saved_patients_entities = pickle.load(f)
     return saved_patients_entities
