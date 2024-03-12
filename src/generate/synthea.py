094588d0c341d05c51ae275e3129998419cdf8c5
xiyaozhuang <114228792+xiyaozhuang@users.noreply.github.com>
2024-03-12 15:26:36 +0000

    Removed automatic write to file

--- src/generate/synthea.py
+++ src/generate/synthea.py
@@ -1,9 +1,9 @@
-from .config import path_synthea_output
-from .utils import run_subprocess, csvs_to_df, df_to_json, save
+from .utils import run_subprocess, csvs_to_df, df_to_json
 
 
 def run(*commands):
     run_subprocess(commands)
     df = csvs_to_df()
-    data = df_to_json(df)
-    save(path_synthea_output, data)
+    output = df_to_json(df)
+
+    return output
